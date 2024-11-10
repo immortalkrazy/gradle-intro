027e35d16e0de509fb9794c42688ff8c78cd45c1
Faisal Ibrahim <faisal.ibrahim@gmail.com>
2024-11-09 22:16:02 -0500

    initial commit

---
+++ src/test/java/com/faisalibrahim/GradleTutorialTest.java
@@ -0,0 +1,12 @@
+package com.faisalibrahim;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+}
+
